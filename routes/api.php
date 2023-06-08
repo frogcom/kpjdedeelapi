007e2b199fa6250103885935e4a77abb6da57330
frogcom <210587@student.glu.nl>
2023-06-08 21:14:19 +0200

    api auth controller

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,10 @@ use App\Http\Controllers\UserController;
 Route::resource('blogs', BlogController::class);
 Route::resource('users', UserController::class);
 
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
